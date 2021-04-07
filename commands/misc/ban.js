22a237b5830991a554c92ecd13677abad29b8e13
LMF <LMFielding@Liams-MacBook-Pro.local>
2021-04-08 00:10:05 +0100

    Tweak.

--- commands/misc/ban.js
+++ commands/misc/ban.js
@@ -3,27 +3,39 @@ import MessagesHelper from '../../core/entities/messages/messagesHelper';
 import STATE from '../../core/state';
 
 
-export default class KickCommand extends CoopCommand {
+export default class BanCommand extends CoopCommand {
 
 	constructor(client) {
 		super(client, {
-			name: 'misc',
+			name: 'ban',
 			group: 'misc',
-			memberName: 'misc',
+			memberName: 'ban',
 			aliases: [],
-			description: '!misc information is classified',
+			description: '!ban information is classified',
 			details: `Details`,
-			examples: ['misc', 'misc example?'],
+			examples: ['ban', 'ban example?'],
 		});
 	}
 
 	async run(msg) {
         super.run(msg);
 
-        MessagesHelper.selfDestruct(msg, 'You are being banned in 3...', 333);
-        MessagesHelper.selfDestruct(msg, 'You are being banned in 2...', 1333);
-        MessagesHelper.selfDestruct(msg, 'You are being banned in 1...', 2333);
-        MessagesHelper.selfDestruct(msg, '!kick is a social construct.', 3333);
+        // Check if it is sefy trying the command.
+
+        // If it is sefy
+        // You are being banned in 2...
+        // You are being banned in 1...
+        // sefy does not have the power to ban you.
+
+        if (msg.author.id !== '208938112720568320') return msg.say('Only sefy can pretend to ban people.')
+        else {
+            //         const repeatNum = STATE.CHANCE.natural({ min: 1, max: 20 });
+            MessagesHelper.selfDestruct(msg, 'You are being banned in 3...', 333);
+            MessagesHelper.selfDestruct(msg, 'You are being banned in 2...', 1333);
+            MessagesHelper.selfDestruct(msg, 'You are being banned in 1...', 2333);
+            MessagesHelper.selfDestruct(msg, 'sefy cannot ban you.', 3333);
+        }
     }
     
 };
+
