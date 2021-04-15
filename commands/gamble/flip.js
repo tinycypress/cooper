9768c86491fd322b05e6703268b6d576e51d1826
LMF <LMFielding@Liams-MacBook-Pro.local>
2021-04-15 22:18:49 +0100

    Push coin flip progress.

--- commands/gamble/flip.js
+++ commands/gamble/flip.js
@@ -1,5 +1,6 @@
 import CoopCommand from '../../operations/activity/messages/coopCommand';
-import COOP, { STATE } from '../../origin/coop';
+import { authorConfirmationPrompt, firstConfirmPrompt } from '../../operations/common/ui';
+import COOP, { MESSAGES, STATE } from '../../origin/coop';
 
 export default class FlipCommand extends CoopCommand {
 
@@ -22,12 +23,28 @@ export default class FlipCommand extends CoopCommand {
 		});
 	}
 
-	async run(msg) {
+	async run(msg, { amount }) {
 		super.run(msg);
 
+		const userID = msg.author.id;
 
-		// 
+		// Confirm game start and amount
+		const goldCoin = MESSAGES._displayEmojiCode('GOLD_COIN');
+		const confirmMsg = await authorConfirmationPrompt(
+			msg, 
+			`<@${userID}>, you want to flip ${amount}x${goldCoin}?`,
+			userID
+		);
 
+		// Check that the author of the command confirmed it.
+		if (confirmMsg) {
+			const joiner = await firstConfirmPrompt(confirmMsg, 'React to join this game!');
+			if (joiner) {
+				// MESSAGES.silentSelfDestruct(conf)
+				msg.say('Someone joined coinflip! Still work in progress, please try again tomorrow.');
+				console.log(joiner);
+			}
+		}
 
 		// TODO: Take the gambling fee.
 
---
+++ operations/common/ui.js
@@ -0,0 +1,41 @@
+import { MESSAGES, USERS } from "../../origin/coop";
+
+
+
+// Recursive confirmation prompt.
+export const firstConfirmPrompt = async function(msgOrChannelRef, text, confirmEmoji = '👍', fuseMs = 20000) {
+    const proceedfeedbackReactFilter = ({ emoji }, user) => 
+        !USERS.isCooper(user.id) && emoji.name == confirmEmoji;
+
+    // Create confirmation with emoji prompt.
+    const promptMsg = await MESSAGES.silentSelfDestruct(msgOrChannelRef, text, 0, fuseMs);
+	MESSAGES.delayReact(promptMsg, confirmEmoji, 222);
+
+	const promptReactions = await promptMsg.awaitReactions(
+		proceedfeedbackReactFilter, 
+		{ max: 1, time: 30000 }
+	);
+	const firstReaction = promptReactions.first();
+	if (firstReaction) return firstReaction;
+    else return false;
+}
+
+
+
+// Recursive confirmation prompt.
+export const authorConfirmationPrompt = async function(msgOrChannelRef, text, userID, confirmEmoji = '👍', fuseMs = 20000) {
+    const proceedfeedbackReactFilter = ({ emoji }, user) => 
+        user.id === userID && emoji.name == confirmEmoji;
+
+    // Create confirmation with emoji prompt.
+    const promptMsg = await MESSAGES.silentSelfDestruct(msgOrChannelRef, text, 0, fuseMs);
+	MESSAGES.delayReact(promptMsg, confirmEmoji, 222);
+
+	const promptReactions = await promptMsg.awaitReactions(
+		proceedfeedbackReactFilter, 
+		{ max: 1, time: 30000 }
+	);
+	const firstReaction = promptReactions.first().emoji.name;
+	if (firstReaction === confirmEmoji) return true;
+    else return false;
+}
