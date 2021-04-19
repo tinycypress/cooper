2938bf42906d1cededac5f13deda7124c481fe19
LMF <LMFielding@Liams-MacBook-Pro.local>
2021-04-19 18:44:14 +0100

    Start floating.

--- commands/economy/use.js
+++ commands/economy/use.js
@@ -4,7 +4,7 @@ import ShieldHandler from '../../operations/minigames/medium/economy/items/handl
 import RPGHandler from '../../operations/minigames/medium/economy/items/handlers/rpgHandler';
 import EasterEggHandler from '../../operations/minigames/medium/economy/items/handlers/easterEggHandler';
 
-import { itemCodeArg, itemQtyArg } from '../../operations/minigames/medium/economy/itemCmdGuards';
+import { itemCodeArg, itemQtyArg, validItemQtyArgFloatGuard } from '../../operations/minigames/medium/economy/itemCmdGuards';
 
 import CoopCommand from '../../operations/activity/messages/coopCommand';
 import COOP from '../../origin/coop';
@@ -27,9 +27,10 @@ export default class UseCommand extends CoopCommand {
 	async run(msg, { itemCode, qty }) {
 		super.run(msg);
 
-		// Fix to 1 for now until further tested.
-		qty = 1;
-		console.log('Tried to !use qty: ' + qty);
+		// Guard to a proper float input.
+		if (!validItemQtyArgFloatGuard(qty))
+			return null;
+
 
 		// Interpret item code from text/string/emoji/item_code.
 		itemCode = COOP.ITEMS.interpretItemCodeArg(itemCode);
