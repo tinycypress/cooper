ae465afe445938cd76f430cbe07688c463223a60
LMF <LMFielding@Liams-MacBook-Pro.local>
2021-04-26 23:41:02 +0100

    Add !bans command to count number of bans.

--- commands/community/bans.js
+++ commands/community/bans.js
@@ -3,17 +3,15 @@ import CoopCommand from '../../operations/activity/messages/coopCommand';
 import COOP, { SERVER } from '../../origin/coop';
 import { EMOJIS } from '../../origin/config';
 
-export default class CountCommand extends CoopCommand {
+export default class BansCommand extends CoopCommand {
 
 	constructor(client) {
 		super(client, {
-			name: 'count',
+			name: 'bans',
 			group: 'community',
-			memberName: 'count',
-			aliases: ['numusers', 'serversize', 'beaks'],
-			description: 'Get the current member count',
-			details: ``,
-			examples: ['count', 'count example']
+			memberName: 'bans',
+			aliases: ['exbeaks', 'exuers', 'exmembers', 'bannedbeaks'],
+			description: 'Get the current banned member count',
 		});
 	}
 
@@ -23,8 +21,12 @@ export default class CountCommand extends CoopCommand {
 		try {
 			// Delete after sixty seconds.
             const emojiText = COOP.MESSAGES.emojiText(EMOJIS.COOP);
-            const userCount = SERVER._coop().memberCount || 0;
-            const countText = `${userCount} #beaks presently banned from The Coop ${emojiText}!`;
+			
+			// TODO: Calculate as a ratio?
+            // const userCount = SERVER._coop().memberCount || 0;
+			
+			const userBansNum = (await SERVER._coop().fetchBans()).size || 0;
+            const countText = `${userBansNum} #beaks presently banned from The Coop ${emojiText}!`;
 			COOP.MESSAGES.selfDestruct(msg, countText, 0, 20000);
 
 		} catch(e) {
--- commands/community/unban.js
+++ commands/community/unban.js
@@ -32,16 +32,13 @@ export default class UnbanCommand extends CoopCommand {
 		try {
 			// Prevent usage of unban for another hour.
 			const userBans = await SERVER._coop().fetchBans();
-			
-			console.log(userBans);
-
+		
 			// Show the ban info on the unban reaction collector for consent/safety.
 			// Add the suggestion reactions for voting.
 			const userBan = userBans.find(user => user.id === discordID);
+			if (!userBan) return MESSAGES.silentSelfDestruct(msg, 'Could not find that user to unban/ban does not exist.');
 
-			console.log('user ban start');
 			console.log(userBan);
-			console.log('user ban end');
 
 			const banReason = userBan ? 'Ban reason.' : 'Unknown ban reason.';
 			const unbanVoteText = `Vote on unbanning <@${discordID}>, press ${VOTE_FOR} to vote unban.`;
@@ -50,35 +47,28 @@ export default class UnbanCommand extends CoopCommand {
 			
 			// Create a function for updating the consent message during voting.
 			const modifierFn = (msg, { id }, vote) => msg.edit(`${msg.content} \n${vote} <@${id}>`);
+
 			// Calculate the result of the multi-member consent/approval vote.
 			const consentResult = await REACTIONS._usersEmojisAwait(unbanConsentMsg, [VOTE_FOR], modifierFn);			
 			const unbanVotesReq = VotingHelper.getNumRequired(SACRIFICE_RATIO_PERC);
 			const forCount = REACTIONS.countTypeCollection(consentResult, VOTE_FOR) - 1;
 			const votesSufficient = forCount >= unbanVotesReq;
 
-			// console.log(userBan);
-
 			console.log(consentResult);
-
 			console.log(forCount);
 			console.log(votesSufficient);
-
-			// Form the result text and output.
-			let resultText = `Unban vote ${votesSufficient ? 'successful' : 'failed'} ` +
-				`${forCount}/${unbanVotesReq} ${VOTE_FOR}. (WORK IN PROGRESS)`;
 
 			// Unban a user by ID (or with a user/guild member object)
 			// const unbanResult = await SERVER._coop().members.unban(user.id);
 			// 	.then(user => console.log(`Unbanned ${user.username} from ${guild.name}`))
 			// 	.catch(console.error);
 
+			// Form the result text and output.
+			const resultText = `Unban vote ${votesSufficient ? 'successful' : 'failed'} ` +
+				`${forCount}/${unbanVotesReq} ${VOTE_FOR}. (WORK IN PROGRESS)`;
 			return MESSAGES.selfDestruct(msg, resultText, 0, 20000);
 
 		} catch(e) {
-			if (e.message === 'Unknown Ban') {
-				return MESSAGES.silentSelfDestruct(msg, 'Could not find that user to unban/ban does not exist.');
-			}
-
 			console.log('Democratic unban failed.');
 			console.error(e);
 		}
