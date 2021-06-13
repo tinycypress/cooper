f21c82a5be64307318b8b77a18e7d23fb2b63aeb
LMF <LMFielding@Liams-MacBook-Pro.local>
2021-06-13 19:25:14 +0100

    Switch to yarn.

--- api.js
+++ api.js
@@ -4,6 +4,8 @@ import Database from './origin/setup/database';
 // Run the web api.
 bootstrap();
 
+var API_BASE_URL = 'https://cooperchickenbot.herokuapp.com/';
+
 // Define the web api.
 export default async function bootstrap() {
   // Connect to PostGres Database and attach event/error handlers.
