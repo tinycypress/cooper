991fcfbc5eb4f34f503457b2d9c724778df6529c
LMF <LMFielding@Liams-MacBook-Pro.local>
2021-06-13 19:55:43 +0100

    Update api.

--- api.js
+++ api.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import Database from './origin/setup/database';
 
+
+// Dev quick copy and paste oauth url:
+// https://discord.com/api/oauth2/authorize?client_id=799695179623432222&redirect_uri=https%3A%2F%2Fapi.thecoop.group%2Fchallenge-discord-identity&response_type=code&scope=identify
+
 // Run the web api.
 bootstrap();
 
+// Try to find the most direct way to make global (hence var here).
 var API_BASE_URL = 'https://cooperchickenbot.herokuapp.com/';
 
 // Define the web api.
@@ -14,6 +19,13 @@ export default async function bootstrap() {
   const app = express();
 
   app.get('/', (req, res) => res.send('Hello World!'));
+
+  app.get('/challenge-discord-identity', (req, res) => {
+    console.log(req);
+
+    res.send('Hello Discord!')
+  });
+
   
   app.listen(process.env.PORT);
 }
