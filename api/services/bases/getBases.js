78a67200715967bc411b458fa7e711fc376dc6ef
LMF <LMFielding@Liams-MacBook-Pro.local>
2021-06-14 01:33:49 +0100

    Fix fuck up.

--- api/services/bases/getBases.js
+++ api/services/bases/getBases.js
@@ -1,6 +1,6 @@
 import BaseHelper from "../../../operations/minigames/medium/conquest/baseHelper";
 
-export default function getBases(req, res) {
+export default async function getBases(req, res) {
     const bases = await BaseHelper.all();
     return res.status(200).json(bases);
 }
