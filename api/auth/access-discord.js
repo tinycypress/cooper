8d25448ba729db714401c6b6e2ea82f08c76929d
LMF <LMFielding@Liams-MacBook-Pro.local>
2021-06-18 00:50:20 +0100

    Debug passed code.

--- api/auth/access-discord.js
+++ api/auth/access-discord.js
@@ -4,59 +4,62 @@ import Auth from './_auth';
 
 // https://stackoverflow.com/questions/54387939/how-to-actually-use-the-data-requested-using-discord-oauth2
 // https://discordjs.guide/oauth2/#authorization-code-grant-flow
-export default async function AccessDiscord({ query }, res) {
-	const { code } = query;
+export default async function AccessDiscord(req, res) {
 	const result = { success: false, token: null };
 
 	// TODO: Code is not going to be in query params but as post data
 
 	console.log('post request to access discord received.');
 
+	console.log(req.body);
 
-	if (code) {
-		try {
-			const tokenResponse = await axios.post('https://discord.com/api/oauth2/token', 
-				new URLSearchParams({
-					client_id: process.env.DISCORD_APPID,
-					client_secret: process.env.DISCORD_CLIENT_SECRET,
-					code,
-					grant_type: 'authorization_code',
-					redirect_uri: `https://thecoop.group/auth/discord-oauth`,
-					scope: 'identify'
-				}),
-				{
-					headers: {
-						'Content-Type': 'application/x-www-form-urlencoded',
-					},
-				}
-			);
+	const code = req.body.code || null;
+	console.log(code);
 
-			console.log(tokenResponse);
-			console.log(tokenResponse.data);
+	try {
+		if  (!code) throw new Error('No code provided');
 
-			const authData = tokenResponse.data;
-			
-			console.log(authData);
+		const tokenResponse = await axios.post('https://discord.com/api/oauth2/token', 
+			new URLSearchParams({
+				client_id: process.env.DISCORD_APPID,
+				client_secret: process.env.DISCORD_CLIENT_SECRET,
+				code,
+				grant_type: 'authorization_code',
+				redirect_uri: `https://thecoop.group/auth/discord-oauth`,
+				scope: 'identify'
+			}),
+			{
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded',
+				},
+			}
+		);
 
-			// Generate token for debugging.
-			const token = Auth.token();
+		console.log(tokenResponse);
+		console.log(tokenResponse.data);
 
-			console.log(token);
+		const authData = tokenResponse.data;
+		
+		console.log(authData);
 
-			// Validate.
-			result.token = token;
-			result.success = true;
+		// Generate token for debugging.
+		const token = Auth.token();
 
-		} catch (error) {
-			// NOTE: An unauthorized token will not throw an error;
-			// it will return a 401 Unauthorized response in the try block above
-			console.error(error);
-			
-            // TODO: Add the error that will not be thrown to handle in one catch?
+		console.log(token);
 
-			result.success = false;
-			result.error = error.message;
-		}
+		// Validate.
+		result.token = token;
+		result.success = true;
+
+	} catch (error) {
+		// NOTE: An unauthorized token will not throw an error;
+		// it will return a 401 Unauthorized response in the try block above
+		console.error(error);
+		
+		// TODO: Add the error that will not be thrown to handle in one catch?
+
+		result.success = false;
+		result.error = error.message;
 	}
 	
 	// TODO: Figure out what the user is actually sent at this stage?
--- api/auth/authRouter.js
+++ api/auth/authRouter.js
@@ -9,8 +9,9 @@ AuthRouter.post('/access-discord', AccessDiscord);
 // TODO: Test this and remove it, prove the guard is working for protected data.
 AuthRouter.post('/authedonly', Auth.guard(), (req, res) => 
     res.status(200).json({
-        protected: true,
-        member_only: true
+        girrafe: true,
+        member_only: true,
+        testing: 'absolutely'
     })
 );
 
