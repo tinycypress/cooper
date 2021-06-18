7f0db0c696df83f3adbe77857a14020e084d93ed
LMF <LMFielding@Liams-MacBook-Pro.local>
2021-06-18 22:59:34 +0100

    Line up endpoints properly.

--- api/auth/authRouter.js
+++ api/auth/authRouter.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
+import Access from "./access";
 import AccessDiscord from "./access-discord";
 import Auth from "./_auth";
 
 const AuthRouter = Router();
 
-AuthRouter.post('/access-discord', AccessDiscord);
 
-// Add Cooper DM the same way tomorrow!! :D
-// AuthRouter.post('/access-discord', AccessDiscord);
+// Route which handles authentication via Discord oAuth but also Cooper DMs.
+AuthRouter.post('/access', Access);
 
+// An endpoint mostly related to session/user data during-around authentication.
 AuthRouter.get('/me', Auth.guard(), (req, res) => {
     console.log(req.user);
     res.status(200).json({ 
@@ -17,6 +18,9 @@ AuthRouter.get('/me', Auth.guard(), (req, res) => {
     });
 });
 
+
+// TODO: ...
+// Stupid... remove from here and nuxt at next available opportunity!!!!!
 AuthRouter.get('/logout', Auth.guard(), (req, res) => {
     res.status(200).json({ logout: 'success' });
 });
