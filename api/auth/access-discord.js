fc94ad0d0e5e3f45b24ab08643dc74615ec61c66
LMF <LMFielding@Liams-MacBook-Pro.local>
2021-06-18 01:29:29 +0100

    Support auth more backend.

--- api/auth/access-discord.js
+++ api/auth/access-discord.js
@@ -2,6 +2,19 @@ import axios from 'axios';
 import Auth from './_auth';
 
 
+const authorizeDiscord = async () => 
+	axios.post('https://discord.com/api/oauth2/token', 
+		new URLSearchParams({
+			client_id: process.env.DISCORD_APPID,
+			client_secret: process.env.DISCORD_CLIENT_SECRET,
+			code,
+			grant_type: 'authorization_code',
+			redirect_uri: `https://thecoop.group/auth/discord-oauth`,
+			scope: 'identify'
+		}),
+		{ headers: {  'Content-Type': 'application/x-www-form-urlencoded' }}
+	);
+
 // https://stackoverflow.com/questions/54387939/how-to-actually-use-the-data-requested-using-discord-oauth2
 // https://discordjs.guide/oauth2/#authorization-code-grant-flow
 export default async function AccessDiscord(req, res) {
@@ -11,49 +24,33 @@ export default async function AccessDiscord(req, res) {
 		const code = req.body.code || null;
 		if (!code) throw new Error('No code provided');
 
-		const tokenResponse = await axios.post('https://discord.com/api/oauth2/token', 
-			new URLSearchParams({
-				client_id: process.env.DISCORD_APPID,
-				client_secret: process.env.DISCORD_CLIENT_SECRET,
-				code,
-				grant_type: 'authorization_code',
-				redirect_uri: `https://thecoop.group/auth/discord-oauth`,
-				scope: 'identify'
-			}),
-			{
-				headers: {
-					'Content-Type': 'application/x-www-form-urlencoded',
-				},
-			}
-		);
+		const tokenResponse = await authorizeDiscord(code);
 
 		console.log(tokenResponse);
-		console.log(tokenResponse.data);
-
-		const authData = tokenResponse.data;
-		
+		console.log(tokenResponse.data);		
 		console.log(authData);
 
+		// TODO: Ensure we prove this is AUTHORIZED them.
+		
 		// Generate token for debugging.
+		const authData = tokenResponse.data;
 		const token = Auth.token();
 
-		console.log(token);
-
 		// Validate.
+		result.user = {
+			username: 'unknown',
+			status: 'this is a work in progress'
+		}
 		result.token = token;
 		result.success = true;
 
 	} catch (error) {
-		// NOTE: An unauthorized token will not throw an error;
-		// it will return a 401 Unauthorized response in the try block above
 		console.error(error);
-		
-		// TODO: Add the error that will not be thrown to handle in one catch?
 
 		result.success = false;
 		result.error = error.message;
 	}
 	
-	// TODO: Figure out what the user is actually sent at this stage?
-	res.status(200).json(result);
+	// Return successful/errorful auth response. =]
+	return res.status(200).json(result);
 }
