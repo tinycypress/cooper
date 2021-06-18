d2bdffa3f960ab951069aec1a0860b215005777c
LMF <LMFielding@Liams-MacBook-Pro.local>
2021-06-18 03:49:37 +0100

    duhhhh

--- api/auth/authRouter.js
+++ api/auth/authRouter.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import AccessDiscord from "./access-discord";
-import passport from 'passport';
+import Auth from "./_auth";
 
 const AuthRouter = Router();
 
 AuthRouter.post('/access-discord', AccessDiscord);
 
 // TODO: Test this and remove it, prove the guard is working for protected data.
-AuthRouter.get('/authedonly', passport.authenticate('jwt', { session: false }, (req, res) => 
+AuthRouter.get('/authedonly', Auth.guard(), (req, res) => 
     res.status(200).json({
         girrafe: true,
         member_only: true,
