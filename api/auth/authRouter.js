91c938c969a21a1064afb97ea50860889bc1eb4e
LMF <LMFielding@Liams-MacBook-Pro.local>
2021-06-18 12:11:03 +0100

    The final countdown...

--- api/auth/authRouter.js
+++ api/auth/authRouter.js
@@ -9,15 +9,13 @@ AuthRouter.post('/access-discord', AccessDiscord);
 // Add Cooper DM the same way tomorrow!! :D
 // AuthRouter.post('/access-discord', AccessDiscord);
 
-AuthRouter.get('/me', Auth.guard(), (req, res) =>
+AuthRouter.get('/me', Auth.guard(), (req, res) => {
+    console.log(req.user);
     res.status(200).json({
-        // Format according to what Nuxt-auth expects... lol.
-        user: {
-            id: req.user.id,
-            username: req.user.username
-        }
+        id: req.user.id,
+        username: req.user.username
     })
-);
+});
 
 // AuthRouter.get('/authedonly', Auth.guard(), (req, res) => 
 //     res.status(200).json({
