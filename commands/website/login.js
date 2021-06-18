e996a1e3ebf966a1f82a3872ce1eb064eb8d0f40
LMF <LMFielding@Liams-MacBook-Pro.local>
2021-06-18 22:26:48 +0100

    =]

--- commands/website/login.js
+++ commands/website/login.js
@@ -25,9 +25,11 @@ export default class LoginCommand extends CoopCommand {
 		// DM the login code to the user
 		USERS._dm(msg.author.id, 
 			`**Your temporary login code (expiring link) is here, use it within the next 5 minutes:**\n\n` +
-			'<https://yougotpunked.net?code=' + code + '>'
+			'https://thecoop.group/auth/login?code=' + code
 		);
 
+		// Also a way to ensure that most codes are deleted in a more timely manner and
+		// also don't require a super fast setInterval =]
 		// TODO: Enhancement When it expires check if it was used/deleted - if not warn them (via editing the msg...?)
 		// setTimeout(() => {
 
--- operations/members/tempAccessCodeHelper.js
+++ operations/members/tempAccessCodeHelper.js
@@ -5,6 +5,23 @@ export default class TempAccessCodeHelper {
 
     static expiry = 60 * 5;
 
+    // Delete all access codes for a certain user (heavy-handed/overkill for security/safety).
+    static delete(discord_id) {
+        return Database.query({
+            text: `DELETE FROM temp_login_codes WHERE discord_id = $1`,
+            values: [discord_id]
+        });
+    }
+
+    static validate(discord_id, code) {
+        // Check code is correct
+
+        // Check it has not expired
+
+        // Validate: Return true/false.
+        return true;
+    }
+
     static async create(discord_id) {
         const code = STATE.CHANCE.string({ length: 10, pool: process.env.DISCORD_TOKEN });
         const expiry = TIME._secs() + this.expiry;
