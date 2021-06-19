1a49e72503a552b84fd5c7bc30c47ce824451f21
LMF <LMFielding@Liams-MacBook-Pro.local>
2021-06-19 01:41:03 +0100

    Improve a lil.

--- origin/setup/slashcommands/login.js
+++ origin/setup/slashcommands/login.js
@@ -17,9 +17,12 @@ export default class LoginCommand extends SlashCommand {
 		const code = await TempAccessCodeHelper.create(ctx.user.id);
 
 		// DM the login code to the user
-		return USERS._dm(ctx.user.id, 
+		USERS._dm(ctx.user.id, 
 			`**Your temporary login code (expiring link) is here, use it within the next 5 minutes:**\n\n` +
 			'https://thecoop.group/auth/authorise?method=cooper_dm&code=' + code
 		);
+
+        // Return string for slash command? :think:
+        return 'Sent you your login link. ;)';
     }
 }
