24b8b43cb71800ff1dbc2c0d7d713cfe2ba0e8c4
LMF <LMFielding@Liams-MacBook-Pro.local>
2021-06-22 01:07:47 +0100

    Secure a lil better.

--- commands/conquest/base.js
+++ commands/conquest/base.js
@@ -29,12 +29,15 @@ export default class BaseCommand extends CoopCommand {
 		super.run(msg);
 		
 		const base = await BaseHelper.get(baseID);
+		if (!base)
+			MESSAGES.silentSelfDestruct(msg, 'There is no base on face id #' + baseID);
+
 		const baseMsgText = `**Tile Details #${base.face_id}:**\n` +
 			`Owner: ${USERS._id2username(base.owner_id)}\n` +
 			`Age: ${base.created_at}`;
 
 		// Sometimes include a video of their base.
-		if (STATE.CHANCE.bool({ likelihood: 5 })) {
+		if (STATE.CHANCE.bool({ likelihood: 2.5 })) {
 			await VisualisationHelper.record("https://www.thecoop.group/conquest/world?tile=" + baseID);
             msg.channel.send(new MessageAttachment('/tmp/video.webm'));
 		}
