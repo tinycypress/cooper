ba844a91cb84248fc7306c5563da2f216db4dd91
LMF <LMFielding@Liams-MacBook-Pro.local>
2021-06-23 20:40:13 +0100

    Allow CORS connection to websocket.

--- api/services/socket/configure.js
+++ api/services/socket/configure.js
@@ -4,7 +4,8 @@ import Socket from "./socket";
 export default function configureWS(server) {
   // Create an instance with reference to socket io server.
   Socket.ws = new Server(server, {
-    serveClient: false
+    serveClient: false,
+    cors: { origin: '*' }
   });
 
   // Handle incoming connections, mainly here for debugging.
