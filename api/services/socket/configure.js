b3f1cbeb356cf8108eaf8d5dc13839137216c4b5
LMF <LMFielding@Liams-MacBook-Pro.local>
2021-06-23 22:10:23 +0100

    Send api refactor at same time.

--- api/services/socket/configure.js
+++ api/services/socket/configure.js
@@ -1,6 +1,25 @@
 import { Server } from "socket.io";
 import Socket from "./socket";
 
+// Remove players after a few minutes of inactivity?
+const players = [];
+
+const playerConnected = socket => {
+  const player = {
+    id: socket.id,
+
+    // TODO: Randomise these two properties and check no conflict/identical.
+    color: 'red',
+    position: { x: 0, y: 0 }
+  }
+
+  // Start tracking new player.
+  players.push(player);
+
+  // Inform all users someone connected.
+  Socket.ws.emit('player_recognised', player);
+}
+
 export default function configureWS(server) {
   // Create an instance with reference to socket io server.
   Socket.ws = new Server(server, {
@@ -8,29 +27,8 @@ export default function configureWS(server) {
     cors: { origin: '*' }
   });
 
-  // Remove players after a few minutes of inactivity?
-  const players = [];
-
   // Handle incoming connections, mainly here for debugging.
-  Socket.ws.on('connection', (socket) => {
-    console.log('a user connected');
-    const player = {
-      id: socket.id
-    }
-
-    // Start tracking new player.
-    players.push(player);
-
-    // Inform all users someone connected.
-    Socket.ws.emit('player_recognised', {
-      // TODO: Randomise these two properties and check no conflict/identical.
-      color: 'red',
-      position: {
-        x: 0,
-        y: 0
-      }
-    });
-  });
+  Socket.ws.on('connection', playerConnected);
 
   // TODO:
   // Add an event listener for moving which broadcasts to all other users.
