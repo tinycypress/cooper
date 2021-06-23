1e8f5c9ac0732f12d5c8fe9fa03a5a66c4d32dd5
LMF <LMFielding@Liams-MacBook-Pro.local>
2021-06-23 23:12:02 +0100

    FUUUUUUUUUUUUUUUCK u.

--- api/services/socket/configure.js
+++ api/services/socket/configure.js
@@ -7,7 +7,7 @@ const players = [];
 const tileLength = 15;
 
 const getRandomColor = () => {
-  const letters = '0123456789ABCDEF';
+  const letters = '0123456789abcdef';
   let color = '0x';
   for (let i = 0; i < 6; i++) color += letters[Math.floor(Math.random() * 16)];
   return color;
