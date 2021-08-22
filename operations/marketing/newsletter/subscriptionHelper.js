2edcfbc30123bd03e3a1d9441517a58125ecb5f6
LMF <contact@lmfielding.com>
2021-08-22 03:39:00 +0100

    Add new subscription method.

--- operations/marketing/newsletter/subscriptionHelper.js
+++ operations/marketing/newsletter/subscriptionHelper.js
@@ -15,73 +15,60 @@ export default class SubscriptionHelper {
 
     // Check if email address is within message, if so... add it for them.
     static async onMessage(msg) {
-        if (msg.channel.type === 'dm' && !COOP.USERS.isCooperMsg(msg)) {
-            const email = this.getEmailFromMessage(msg);
-            if (email) {
-                const detectText = '**Email Address Detected:**\n';
-                const actionText = `You seem to be trying to add or update your Coop email address. \n\n`;
-                const termsText = `**You may unsubscribe** at any time using the **!unsubscribe command**, ` +
-                    `your email will not be visible to anyone and is used solely for our newsletter/relay. \n\n` +
-                    `_If you would like your email to be public, for example a business email - we will make this possible soon!_`;
-                const confirmText = `\n\nPlease confirm ${email} is your email in the next 60 secs using the emoji reactions!`;
+        // Only allow in DMs.
+        if (msg.channel.type !== 'dm') return null;
 
-                const confirmEmailText = detectText + actionText + termsText + confirmText;
-                const confirmMsg = await msg.say(confirmEmailText);
+        // Ignore Cooper for this.
+        if (COOP.USERS.isCooperMsg(msg)) return null;
 
-                // Avoid rate limiting/hammering network.
-                COOP.MESSAGES.delayReact(confirmMsg, EMOJIS.POLL_FOR, 333);
-                COOP.MESSAGES.delayReact(confirmMsg, EMOJIS.POLL_AGAINST, 666);
-                setTimeout(() => {
+        const email = this.getEmailFromMessage(msg);
+        if (email) {
+            const detectText = '**Email Address Detected:**\n';
+            const actionText = `You seem to be trying to add or update your Coop email address. \n\n`;
+            const termsText = `**You may unsubscribe** at any time using the **!unsubscribe command**, ` +
+                `your email will not be visible to anyone and is used solely for our newsletter/relay. \n\n` +
+                `_If you would like your email to be public, for example a business email - we will make this possible soon!_`;
+            const confirmText = `\n\nPlease confirm ${email} is your email in the next 60 secs using the emoji reactions!`;
+
+            const confirmEmailText = detectText + actionText + termsText + confirmText;
+            const confirmMsg = await msg.say(confirmEmailText);
+
+            // Avoid rate limiting/hammering network.
+            COOP.MESSAGES.delayReact(confirmMsg, EMOJIS.POLL_FOR, 333);
+            COOP.MESSAGES.delayReact(confirmMsg, EMOJIS.POLL_AGAINST, 666);
+
+            setTimeout(async () => {
+                try {
                     // Await approval for addition.
-                    confirmMsg.awaitReactions(
-                        (reaction, user) => {
-                            return (
-                                !COOP.USERS.isCooper(user.id) &&
-                                [EMOJIS.POLL_FOR, EMOJIS.POLL_AGAINST].includes(reaction.emoji.name)
-                            );
-                        }, 
+                    const collected = await confirmMsg.awaitReactions((reaction, user) => (
+                            !COOP.USERS.isCooper(user.id) &&
+                            [EMOJIS.POLL_FOR, EMOJIS.POLL_AGAINST].includes(reaction.emoji.name)
+                        ), 
                         { max: 1, time: 60000, errors: ['time'] }
-                    )
-                        .then(async (collected) => {
-                            const reaction = collected.first();
+                    );
+
+                    const reaction = collected.first();
+                    const confirmed = reaction.emoji.name === EMOJIS.POLL_FOR;
+
+                    if (!confirmed) return confirmMsg.reply('Submission declined.');
+
+                    // Add their email to database.
+                    const subscription = await this.subscribe(msg.author.id, email);
+
+                    // Handle new subscription.
+                    if (subscription.success && subscription.newLead) 
+                        confirmMsg.reply('Thank you for subscribing via email.');
                     
-                            if (reaction.emoji.name === EMOJIS.POLL_FOR) {
-
-                                // Acknowledge receipt of confirmation.
-                                await confirmMsg.reply('Thank you for confirming your email.');
-                                
-                                // Add their email to database.
-                                const subscription = await this.subscribe(msg.author.id, email);
-                                
-                                // // Reward them, but not if their email address was set to an unsubscribed default.
-                                // if (subscription.newLead && subscription.success) {
-                                //     const username = msg.author.username;
-                                //     const rewardText = `Thank you for subscribing ${username}. `;
-                                //     const rewardAmountText = `+25 points, +5 AXE, +5 PICKAXE rewarded!`;
-
-                                //     COOP.ITEMS.add(msg.author.id, 'COOP_POINT', 25, 'Email subscription reward');
-                                //     COOP.ITEMS.add(msg.author.id, 'AXE', 5, 'Email subscription reward');
-                                //     COOP.ITEMS.add(msg.author.id, 'PICK_AXE', 5, 'Email subscription reward');
-
-                                //     setTimeout(async () => {
-                                //         COOP.CHANNELS._postToFeed(rewardText + rewardAmountText);
-                                //         confirmMsg.say(rewardText + rewardAmountText);
-                                //     }, 3000);
-
-                                // // Provide update feedback too!
-                                // } else if (subscription.success) confirmMsg.say('Your email address was updated.');
-                                
-                                if (subscription.success) confirmMsg.say('Your email address was updated.');
-
-                                
-                            } else confirmMsg.reply('You declined to confirm, personal data not saved.');
-                        })
-                        .catch((e) => {
-                            console.error(e);
-                            confirmMsg.reply('Confirmation fail. Try again by stating your email.')
-                        });
-                }, 1333);
-            }
+                    // Handle existing subscription modification.
+                    else if (subscription.success && !subscription.newLead) 
+                        confirmMsg.say('Your email address was updated.');
+                    
+                } catch(e) {
+                    confirmMsg.reply('Confirmation fail. Try again by stating your email.');
+                    console.log('Subscription creation fail.');
+                    console.error(e);
+                }
+            }, 1333);
         }
     }
 
@@ -127,7 +114,7 @@ export default class SubscriptionHelper {
                 this.upgradeAnonSubscription(subscription.id, userID);
 
             // If email was not already known, create a new subscription.
-            if (!subscription) {
+            else if (!subscription) {
                 subscription.newLead = true;
                 this.create(email, userID, 1);
             }
@@ -167,3 +154,22 @@ export default class SubscriptionHelper {
         });
     }
 }
+
+
+// // Reward them, but not if their email address was set to an unsubscribed default.
+// if (subscription.newLead && subscription.success) {
+//     const username = msg.author.username;
+//     const rewardText = `Thank you for subscribing ${username}. `;
+//     const rewardAmountText = `+25 points, +5 AXE, +5 PICKAXE rewarded!`;
+
+//     COOP.ITEMS.add(msg.author.id, 'COOP_POINT', 25, 'Email subscription reward');
+//     COOP.ITEMS.add(msg.author.id, 'AXE', 5, 'Email subscription reward');
+//     COOP.ITEMS.add(msg.author.id, 'PICK_AXE', 5, 'Email subscription reward');
+
+//     setTimeout(async () => {
+//         COOP.CHANNELS._postToFeed(rewardText + rewardAmountText);
+//         confirmMsg.say(rewardText + rewardAmountText);
+//     }, 3000);
+
+// // Provide update feedback too!
+// } else if (subscription.success) confirmMsg.say('Your email address was updated.');
