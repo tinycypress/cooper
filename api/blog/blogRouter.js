e70fe5e2a13d9f775a24763f59ae31a2e82a728b
LMF <contact@lmfielding.com>
2021-08-22 01:48:17 +0100

    Fix error.

--- api/blog/blogRouter.js
+++ api/blog/blogRouter.js
@@ -3,12 +3,14 @@ import BlogHelper from "../../operations/marketing/blog/blogHelper";
 
 const BlogRouter = Router();
 
-BlogRouter.get('/', async (req, res) => 
-    res.status(200).json({ posts: await BlogHelper.loadHeadlines() })
-);
+BlogRouter.get('/', async (req, res) => {
+    const posts = await BlogHelper.loadHeadlines();
+    res.status(200).json({ posts });
+});
 
-BlogRouter.get('/:slug', (req, res) =>
-    res.status(200).json(await BlogHelper.loadPostBySlug(req.params.slug))
-);
+BlogRouter.get('/:slug', (req, res) => {
+    const post = await BlogHelper.loadPostBySlug(req.params.slug);
+    res.status(200).json(post)
+});
 
 export default BlogRouter;
