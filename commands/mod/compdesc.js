ad3dea5f6a8e86aa12f9b1fe296404deea9afd21
L M Fielding <contact@lmfielding.com>
2021-10-02 21:00:12 +0100

    Debug it.

--- commands/mod/compdesc.js
+++ commands/mod/compdesc.js
@@ -31,6 +31,8 @@ export default class CompetitionDescriptionCommand extends CoopCommand {
 	async run(msg, { competition_event_code, description }) {
 		super.run(msg);
 
+		console.log(competition_event_code, description);
+
 		try {
 			// Check if user is leader or commander.
 			if (!ROLES._idHasCode(msg.author.id, 'COMMANDER') && !ROLES._idHasCode(msg.author.id, 'LEADER'))
