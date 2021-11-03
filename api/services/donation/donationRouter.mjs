f017effb843c6b5ed811d6353c3c251f87ef3497
L M Fielding <contact@lmfielding.com>
2021-11-03 18:49:16 +0000

    Add suggest slash command.

--- api/services/donation/donationRouter.mjs
+++ api/services/donation/donationRouter.mjs
@@ -1,48 +1,23 @@
+import _ from 'lodash';
 import { Router } from "express";
 
 const DonationRouter = Router();
 
 DonationRouter.post('/', async (req, res) => {
-    console.log('Donation1?!?!');
-    console.log(req.body);
-
     // process.env.RAISELY_ENCRYPTION_KEY
 
-    console.log(req.body.data.data.user);
-    console.log(req.body.data.data.profile);
-
-    req.body.data.data.receipts.map(r => {
-        console.log(r);
-    });
+    // Access the potential discord ID from webhook payload.
+    const discordID = _.get(req.body, 'data.data.public.discordID');
+    if (discordID) {
+        console.log('Donation with Discord ID', discordID);
+    } else {
+        console.log('Donation without Discord ID');
+    }
+    
+    const data = _.get(req.body, 'data.data');
+    console.log(data);
 
     res.sendStatus(200);
-    // .json({
-    //     "DONATION": "TESTING"
-    // });
 });
 
 export default DonationRouter;
-
-
-// {
-//     "secret": "myverysecretsecret",
-//     "data": {
-//       "uuid": "xxxx-xxxx-xxxx-xxxx", // unique uuid of the event
-//       "type": "profile.updated",
-//       "createdAt": "2018-02-19T00:00:00Z", // ISO8601 timestamp of when the event was created
-//       "source": "campaign:uuid", // where the event originated from in the format of model:uuid
-//       "data": {
-//         "uuid": "xxxx-xxxx-xxxx-xxxx"
-//         // JSON representation of the model
-//       },
-      
-//       "diff": {
-//         "new": {
-//           "description": "My new description"
-//         },
-//         "old": {
-//           "description": "My old description"
-//         }
-//       }
-//     }
-//   }
---
+++ commands/community/suggest.mjs
@@ -0,0 +1,63 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+
+import COOP, { USERS } from '../../origin/coop.mjs';
+import { EMOJIS } from '../../origin/config.mjs';
+import SuggestionsHelper from '../../operations/activity/suggestions/suggestionsHelper.mjs';
+
+export const name = 'suggest';
+
+// description: 'Allows you to suggest Coop related changes to the #suggest channel.',
+export const description = 'Suggest a change or addition to the community/server/services';
+
+// examples: ['!suggest <suggestion>', '!suggest update The Coop blog biweekly'],
+    
+export const data = new SlashCommandBuilder()
+	.setName(name)
+	.setDescription(description)
+	.addStringOption(option => 
+		option
+			.setName('text')
+			.setDescription('Suggestion text/message?')
+			.setRequired(true)
+	)
+
+export const execute = async (interaction) => {
+	let suggestionText = interaction.options.get('text').value ?? '';
+
+	// Make safe to repost content, @isoleucine?
+
+	if (suggestionText.includes('@everyone') || suggestionText.includes('@here'))
+		return COOP.MESSAGES.selfDestruct(interaction.channel, 'Pinging via Cooper disallowed.');
+
+	try {
+		// Post in suggestions.
+		const cleanedContent = suggestionText.replace('!suggestion', '').replace('!suggest', '');
+		const pollAcknowledgement = await COOP.CHANNELS._postToChannelCode('SUGGESTIONS', cleanedContent);
+
+		// Detect attempts to suggest PROJECT_CHANNEL
+		if (suggestionText.toLowerCase().includes('project channel')) {
+			// If needs an owner to give the project channel to.
+		}
+
+		// Add reactions for people to use.
+		SuggestionsHelper.activateSuggestion(pollAcknowledgement);
+
+		// Add intended for roadmap, add roadmap reaction for adding to roadmap.
+		if (suggestionText.toLowerCase().indexOf('roadmap') > -1)
+			COOP.MESSAGES.delayReact(pollAcknowledgement, EMOJIS.ROADMAP, 999);
+
+		// Send poll tracking link.
+		USERS._dm(interaction.user.id, 
+			'I started your poll, track its progress with this link: ' + 
+			COOP.MESSAGES.link(pollAcknowledgement) + 
+			+ " \n\n\n " + " _ " + suggestionText
+		);
+
+		// Send confirmation of success.
+		return await interaction.reply('Suggestion created, more details: suggestions channel / DM');
+
+	} catch(err) {
+		console.error(err);
+		return false;
+	}
+};
