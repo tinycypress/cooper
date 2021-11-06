118d12b87d0e86deb3aecfe112dad873fa554f61
L M Fielding <contact@lmfielding.com>
2021-11-06 00:08:12 +0000

    Attempt to test responding to slash command buttons.

--- commands/blog/post.mjs
+++ commands/blog/post.mjs
@@ -3,8 +3,9 @@ import { SlashCommandBuilder } from "@discordjs/builders";
 import { RAW_EMOJIS, EMOJIS } from '../../origin/config.mjs';
 import { MESSAGES, ITEMS, TIME, USERS, CHANNELS } from '../../origin/coop.mjs';
 
+import BlogHelper from '../../operations/marketing/blog/blogHelper.mjs';
+import Database from '../../origin/setup/database.mjs';
 import UsableItemHelper from '../../operations/minigames/medium/economy/items/usableItemHelper.mjs';
-// import ProjectsHelper from '../../operations/productivity/projects/projectsHelper.mjs';
 
 import { authorConfirmationPrompt } from '../../operations/common/ui.mjs';
 
@@ -45,29 +46,23 @@ export const data = new SlashCommandBuilder()
 			.setDescription('Publish this channel.'));
 
 export const execute = async (interaction) => {
+	const action = interaction.options.getSubcommand();
+	if (action === 'post') return post(interaction);
+	if (action === 'preview') return preview(interaction);
+	if (action === 'publish') return publish(interaction);
+}
 
-	console.log(interaction.isCommand());
-
-	console.log(interaction.options.getSubcommand());
-
-	// New post?
-	// Preview?
-	// Publish?
-
-	return interaction.reply('Testing which sub command fired.');
-
-
-
+const post = interaction => {
 	// Access the project title text.
 	const title = interaction.options.get('title').value ?? '';
 	const deadline = interaction.options.get('deadline').value ?? '';
 
-	// Check deadline is valid.
-	if (!TIME.isValidDeadline(deadline))
-		return MESSAGES.silentSelfDestruct(msg, `<@${msg.author.id}>, ${deadline} is an invalid duration for a post deadline.`);
-
 	// TODO: Check title is valid.
 	// TODO: Check the project does not already exist.
+
+	// Check deadline is valid.
+	if (!TIME.isValidDeadline(deadline))
+		return MESSAGES.selfDestruct(interaction.channel, `<@${interaction.user.id}>, ${deadline} is an invalid duration for a post deadline.`);
 
 	// Calculate the price.
 	const basePrice = await ITEMS.perBeakRelativePrice('GOLD_COIN', 0.05);
@@ -79,7 +74,7 @@ export const execute = async (interaction) => {
 	const createProjectText = '**Create !post?** Details:\n\n' +
 
 		'Title: __' + title + '__\n' +
-		'Writer: ' + `<@${msg.author.id}>` + '\n' +
+		'Writer: ' + `<@${interaction.user.id}>` + '\n' +
 		'Deadline: ' + deadline + '\n' +
 		'Price: ' + emoji + ' ' + price + ' _(0.01% avg coin qty a week)_\n\n'
 
@@ -89,20 +84,20 @@ export const execute = async (interaction) => {
 	const confirmText = createProjectText + '_Please react with tick to propose the blog post\'s creation!_';
 
 	// Check the user can afford to pay the price!
-	const userCoinQty = await ITEMS.getUserItemQty(msg.author.id, 'GOLD_COIN');
+	const userCoinQty = await ITEMS.getUserItemQty(interaction.user.id, 'GOLD_COIN');
 	if (userCoinQty < price)
-		return MESSAGES.silentSelfDestruct(msg, `<@${msg.author.id}>, you cannot afford the post price (${price}xGOLD_COIN).`);
+		return MESSAGES.selfDestruct(interaction.channel, `<@${interaction.user.id}>, you cannot afford the post price (${price}xGOLD_COIN).`);
 
 	// Use the confirmation from the coin flip feature! :D
-	const confirmMsg = await authorConfirmationPrompt(msg, confirmText, msg.author.id);
+	const confirmMsg = await authorConfirmationPrompt(interaction.channel, confirmText, interaction.user.id);
 	if (!confirmMsg) return null;
 
 	// Check the user did pay.
-	const didPay = await UsableItemHelper.use(msg.author.id, 'GOLD_COIN', price, 'Proposing blog post');
-	if (!didPay) return MESSAGES.silentSelfDestruct(msg, `Post proposal cancelled, payment failure.`);
+	const didPay = await UsableItemHelper.use(interaction.user.id, 'GOLD_COIN', price, 'Proposing blog post');
+	if (!didPay) return MESSAGES.selfDestruct(interaction.channel, `Post proposal cancelled, payment failure.`);
 	
 	// Proceed to list the channel for approval.
-	MESSAGES.silentSelfDestruct(msg, title + '\'s blog post channel is being voted on!');
+	MESSAGES.selfDestruct(interaction.channel, title + '\'s blog post channel is being voted on!');
 
 	// Create the project in suggestions for democratic approval.
 	const postSuggestMsg = await CHANNELS._postToChannelCode('SUGGESTIONS', createProjectText);
@@ -115,11 +110,57 @@ export const execute = async (interaction) => {
 	MESSAGES.delayReact(postSuggestMsg, RAW_EMOJIS.POST, 999);
 	
 	// Send poll tracking link.
-	USERS._dm(msg.author.id, 
+	USERS._dm(interaction.user.id, 
 		title + '\'s project channel is being voted on:\n' + 
 		MESSAGES.link(postSuggestMsg)
 	);
 	
 	// Indicate success.
 	return true;
+}
+
+const preview = interaction => {
+	const draft = await BlogHelper.loadDraftByChannelID(interaction.channel.id);
+	const previewLink = `https://thecoop.group/blog/preview?channel_id=${interaction.channel.id}`;
+
+	if (!draft)
+		MESSAGES.selfDestruct(interaction.channel, 'Try preview on a post draft channel!');
+
+	// Add content to the table so it shows up to date.
+	const chan = CHANNELS._get(draft.channel_id);
+	const content = await BlogHelper.buildDraft(chan);
+
+	await Database.query({
+		name: "update-draft-content",
+		text: `UPDATE post_drafts SET content = $1 WHERE channel_id = $2`,
+		values: [content, draft.channel_id]
+	});
+
+	// Send the link
+	MESSAGES.selfDestruct(interaction.channel, `**${draft.title}** preview: \n<${previewLink}>`);
+
+	return interaction.reply('Preview is work in progres.');
+}
+
+const publish = interaction => {		
+	try {
+		// If ID is null... try to see if the current one will work.
+		const draft = await BlogHelper.loadDraftByChannelID(interaction.channel.id);
+		if (!draft)
+			return MESSAGES.selfDestruct(interaction.channel, 'Please run command within a post channel.');
+
+		// Check user is the owner of the blog post draft.
+		if (interaction.user.id !== draft.owner_id)
+			return MESSAGES.selfDestruct(interaction.channel, 'You cannot manage that blog post draft.');
+
+		const confirmMsg = await authorConfirmationPrompt(interaction.channel, 'Really publish ' + draft.title + '?', interaction.user.id);
+		if (!confirmMsg) return null;
+
+		// Fulfil the draft.
+		BlogHelper.fulfilDraft(draft);
+		
+	} catch(e) {
+		console.log('Failed to publish blog post draft.');
+		console.error
+	}
 }
--- commands/website/messageactionrow.mjs
+++ commands/website/messageactionrow.mjs
@@ -13,12 +13,29 @@ export const execute = async (interaction) => {
     const row = new MessageActionRow()
         .addComponents(
             new MessageButton()
-                .setCustomId('primary')
-                .setLabel('Primary')
-                .setStyle('PRIMARY'),
+                .setCustomId('confirm')
+                .setLabel('Confirm')
+                .setStyle('SUCCESS'),
+            new MessageButton()
+                .setCustomId('cancel')
+                .setLabel('Cancel')
+                .setStyle('DANGER'),
         );
+
+    // Defer so we have longer to work/wait for response?
+    interaction.deferUpdate();
     
     await interaction.reply({ content: 'Message action row!', components: [row] });
+
+    const filter = i => !!i;
+
+    const collector = interaction.channel.createMessageComponentCollector({ filter, time: 15000 });
+
+    collector.on('collect', async i => {
+        await i.update({ content: 'A button was clicked!', components: [] });
+    });
+
+    collector.on('end', collected => console.log(`Collected ${collected.size} items`));
 };
 
 
